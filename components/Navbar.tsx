162ecee6a3730b4c9387a6cc91d9c11f2fecff44
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-11-11 23:47:28 +0530

    adding a new Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -270,3 +270,12 @@
 //         </Navbar>
 //     );
 // }
+
+
+export default function Navbar() {
+    return (
+        <nav className="bg-blue-600 p-4 text-white">
+            <h1 className="text-xl font-bold">Nagpal Tour and Travel</h1>
+        </nav>
+    );
+}   
