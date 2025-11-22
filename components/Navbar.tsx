2d40cad5712df278431caa5d322fd675e76ca057
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-11-22 23:12:23 +0530

    changing in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,8 @@
 export default function Navbar() {
     return (
         <nav className="bg-blue-600 p-4 text-white">
-            <h1 className="text-xl font-bold">Nagpal Tour and Travel</h1>
+            <h1 className="text-xl font-bold">Backpackers Ai Suite</h1>
+            <em> Ai approach for tour and travel</em>
         </nav>
     );
 }
