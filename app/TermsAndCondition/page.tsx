c482058ab1a8182ec9ebc4868c893281d9a43797
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-11-26 00:00:19 +0530

    formatting terms and condition pafe

--- app/TermsAndCondition/page.tsx
+++ app/TermsAndCondition/page.tsx
@@ -1,8 +1,8 @@
 
 export default function page() {
-  return (
-    <div>
-        <h1>It is the terms and service page</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>It is the terms and service page</h1>
+        </div>
+    )
 }
